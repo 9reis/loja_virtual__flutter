5d4f9876901f88e46f8c8e6fdc92fbd3cae8667d
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-18 13:17:00 -0300

    Projeto inicializado

---
+++ android/app/src/main/java/br/com/novereistechnology/lojavirtual/loja_virtual__flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.novereistechnology.lojavirtual.loja_virtual__flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
